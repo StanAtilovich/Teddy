89a230709b8197942611a0ffcfdf2fa1d1da8a05
StanAtilovich <alano92@yandex.ru>
2024-01-11 12:55:09 +0800

    Надо это значение в shared preferences сохранять. Тогда будет доступно везде.

--- app/src/main/java/ru/stan/teddy/PreK.kt
+++ app/src/main/java/ru/stan/teddy/PreK.kt
@@ -1,5 +1,6 @@
 package ru.stan.teddy
 
+import android.preference.PreferenceManager
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,16 +11,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+
 
 @Composable
-fun PreK () {
-    val enteredCode = remember { mutableStateOf("") }
+fun PreK() {
+    val context = LocalContext.current
+    val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+    val editor = sharedPreferences.edit()
+
     val correctCode = "234" // Предположим, что правильный код - "234"
+
+    val enteredCode = remember {
+        mutableStateOf(sharedPreferences.getString("enteredCode", "") ?: "")
+    }
 
     Column(
         modifier = Modifier
@@ -29,7 +41,11 @@ fun PreK () {
     ) {
         OutlinedTextField(
             value = enteredCode.value,
-            onValueChange = { enteredCode.value = it },
+            onValueChange = {
+                enteredCode.value = it
+                editor.putString("enteredCode", it)
+                editor.apply()
+            },
             label = { Text("Enter the code") }
         )
 
